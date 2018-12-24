f6ccfde7215502d6914ec50f65c69990980ed891
cuong <caitenkhongbitrung@gmail.com>
2018-12-24 15:00:28 +0700

    for deployment

--- SERVER.js
+++ SERVER.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs');
 app.set("views","./views")
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-server.listen(3000,()=>{ console.log("Server running on port 3000"); });
+server.listen(process.env.PORT,()=>{ console.log("Server running on port 3000"); });
 const util = require('util')
 var RoomsList = {};
 var mangUser = {};
